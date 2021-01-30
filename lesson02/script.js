23e59f7a9eae2b4c63ed4cfda453ff430a9d2f1f
Ivanov Dmitriy <rey.veritas@yandex.ru>
2021-01-30 23:37:33 +0500

    Выполнил усложненное задание из урока 2

--- lesson02/script.js
+++ lesson02/script.js
@@ -1,1 +1,12 @@
 let num = 266219;
+let composition = 1;
+
+let arrayNum = num.toString().split(''); // Преобразование числа в массив цифр
+
+for (let i = 0; i < arrayNum.length; i++) { // Произведение цифр числа
+    composition *= arrayNum[i];
+}
+console.log('Произведение цифр числа =', composition);
+
+let degreeNum = composition ** 3; // Возведение в степень
+console.log('Первые две цифры полученного числа: ', +degreeNum.toString().substr(0, 2));
